ec0aaa45598bc2a097d523b23620a8a5dcdb9cb3
rubi-ekak <125873059+rubi-ekak@users.noreply.github.com>
2023-03-20 12:22:31 +0530

    Add files via upload
    UI changes 20-03-2023 12:22

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,6 +10,7 @@ const About = () => {
             <div className="about-padding">
             <h1 className="about-head">About </h1>
             <p className="about-para">
+              friend
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
               dolores recusandae error rem libero vel. Maiores, eius, alias
               autem earum facilis praesentium quas debitis obcaecati, molestias
